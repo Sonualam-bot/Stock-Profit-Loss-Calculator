e9b1b268e1db88b76d738446084cc4b144cd1e61
Sonu <sonualam1599@gmail.com>
2021-09-25 18:19:49 +0530

    updated css

---
+++ index.js
@@ -0,0 +1,39 @@
+var initialPrice = document.querySelector("#initial-price");
+var stocksQuantity = document.querySelector("#stocks-quantity");
+var currentPrice = document.querySelector("#current-price");
+var showBtn = document.querySelector("#show-btn");
+var outputBox = document.querySelector("#output-box");
+
+
+
+function calculateProfitAndLoss(initial, quantity, current) {
+    if (initial > current) {
+        var loss = Number(initial - current) * quantity;
+        var lossPercentage = (loss / initial) * 100;
+
+        showMessage(`Hey! Your loss is ${loss} and your lossPercentage is ${lossPercentage}%`);
+    } else if (current > initial) {
+        var profit = Number(current - initial) * quantity;
+        var profitPercentage = (profit / initial) * 100
+
+        showMessage(`Hey! your profit is ${profit} and your profitPercentage is ${profitPercentage}%`);
+    } else {
+        showMessage('No pain, No gain')
+    }
+}
+
+
+function showMessage(message) {
+    outputBox.innerHTML = message;
+}
+
+
+function submitHandler() {
+    var init = initialPrice.value;
+    var qty = stocksQuantity.value;
+    var curr = currentPrice.value;
+
+    calculateProfitAndLoss(init, qty, curr);
+}
+
+showBtn.addEventListener('click', submitHandler);
